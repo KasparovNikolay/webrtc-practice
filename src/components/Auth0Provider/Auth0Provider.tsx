6dfc8ae9de08869ea60bba4d7d8c1b1d9fd799b5
Kasaprov <Kasparov19ya@yandex.ru>
2023-03-18 19:32:53 +0300

    add eslint and prettier

--- src/components/Auth0Provider/Auth0Provider.tsx
+++ src/components/Auth0Provider/Auth0Provider.tsx
@@ -3,18 +3,24 @@ import {useNavigate} from "react-router-dom";
 import {Auth0Provider as Auth0ProviderLib} from "@auth0/auth0-react";
 import {constants} from "../../utils";
 
-export const Auth0Provider:FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
+type Auth0Provider = { children: ReactNode | ReactNode[] };
+
+export const Auth0Provider: FC<Auth0Provider> = ({children}) => {
   const navigate = useNavigate();
+
   const onRedirectCallback = (appState: any) => {
     navigate((appState && appState.returnTo) || window.location.pathname);
   };
 
   return (
-    <Auth0ProviderLib     domain={constants.DOMAIN}
-                       clientId={constants.CLIENT_ID}
-                       authorizationParams={{
-                         redirect_uri: constants.LOGOUT_LINK,
-                       }} onRedirectCallback={onRedirectCallback}>
+    <Auth0ProviderLib
+      domain={constants.DOMAIN}
+      clientId={constants.CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: constants.LOGOUT_LINK,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
       {children}
     </Auth0ProviderLib>
   );
--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import {useAuth0} from "@auth0/auth0-react";
 import {constants} from "../../utils";
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
 
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: constants.LOGOUT_LINK } });
+  }
+
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: constants.LOGOUT_LINK } })}>
+    <button onClick={handleLogout}>
       Log Out
     </button>
   );
